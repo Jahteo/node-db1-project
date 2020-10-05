5cc7ecea1e6a8ae685fbb2304088454d9fdceebd
Josh Glantz-Hucks <jahteo@gmail.com>
2020-10-05 13:50:11 -0700

    set up server, began adding accounts-router.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+//Why was this here in the original file???
+//const db = require("../data/dbConfig.js");
+
+const AccountsRouter = require('../accounts/accounts-router');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', AccountsRouter)
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: "alive and well, amigos!"})
+})
 
 module.exports = server;
